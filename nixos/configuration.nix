e4c48f7ef7ea3eabe1f4094f13c3559af1a99737
Vincent Brodin <vincent.brodin21@gmail.com>
2025-11-17 00:04:36 +0100

    Added more packages and fixed some minor problems

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -76,6 +76,47 @@
     };
   };
 
+  programs.tmux = {
+    enable = true;
+    escapeTime = 0;
+    shortcut = "SPACE";
+    baseIndex = 1;
+    keyMode = "vi";
+    terminal = "screen-256color";
+    extraConfig = ''
+      bind -r ^ last-window
+      bind -r k select-pane -U
+      bind -r j select-pane -D
+      bind -r h select-pane -L
+      bind -r l select-pane -R
+
+      set -g status-position top
+      set -g status-justify centre
+      set -g status-style 'fg=colour7 bg=default'
+
+      # left: session name
+      set -g status-left '#S'
+      set -g status-left-style 'fg=colour4'
+      set -g status-left-length 40
+
+      # right: host name
+      set -g status-right '#H'
+      set -g status-right-style 'fg=colour4'
+      set -g status-right-length 40
+
+      # windows: plain text, subtle highlight on current
+      setw -g window-status-format '#I:#W '
+      setw -g window-status-current-format '#[fg=colour4]#I:#W '
+      setw -g window-status-style 'fg=colour7'
+      setw -g window-status-current-style 'fg=colour4'
+
+      # pane borders (thin contrast)
+      set -g pane-border-style 'fg=colour8'
+      set -g pane-active-border-style 'fg=colour4'
+
+    '';
+  };
+
   # Sound server
   security.rtkit.enable = true;
   services.pipewire = {
@@ -104,7 +145,10 @@
       "wheel"
     ];
     shell = pkgs.zsh;
-    packages = with pkgs; [ discord spotify ];
+    packages = with pkgs; [
+      discord
+      spotify
+    ];
   };
 
   nixpkgs.config.allowUnfree = true;
--- nixos/home.nix
+++ nixos/home.nix
@@ -16,68 +16,32 @@
     enable = true;
     enableZshIntegration = true;
   };
-  programs.tmux = {
-    enable = true;
-    escapeTime = 0;
-    shortcut = "SPACE";
-    baseIndex = 1;
-    keyMode = "vi";
-    terminal = "screen-256color";
-    extraConfig = ''
-      bind -r ^ last-window
-      bind -r k select-pane -U
-      bind -r j select-pane -D
-      bind -r h select-pane -L
-      bind -r l select-pane -R
 
-      set -g status-position top
-      set -g status-justify centre
-      set -g status-style 'fg=colour7 bg=default'
-
-      # left: session name
-      set -g status-left '#S'
-      set -g status-left-style 'fg=colour4'
-      set -g status-left-length 40
-
-      # right: host name
-      set -g status-right '#H'
-      set -g status-right-style 'fg=colour4'
-      set -g status-right-length 40
-
-      # windows: plain text, subtle highlight on current
-      setw -g window-status-format '#I:#W '
-      setw -g window-status-current-format '#[fg=colour4]#I:#W '
-      setw -g window-status-style 'fg=colour7'
-      setw -g window-status-current-style 'fg=colour4'
-
-      # pane borders (thin contrast)
-      set -g pane-border-style 'fg=colour8'
-      set -g pane-active-border-style 'fg=colour4'
-
-    '';
-  };
-
-  home.packages = [
+  home.packages = with pkgs; [
     # HELIX
-    pkgs.helix
-    pkgs.nil
-    pkgs.nixfmt-rfc-style
+    helix
+    nil
+    nixfmt-rfc-style
     # RUST
-    pkgs.rustc
-    pkgs.cargo
-    pkgs.rustfmt
-    pkgs.rust-analyzer
+    rustc
+    cargo
+    rustfmt
+    rust-analyzer
     # DEV
-    pkgs.pkg-config
-    pkgs.openssl
-    pkgs.gcc
-    pkgs.gh
-    # MISC
-    pkgs.rofi
-    pkgs.rose-pine-hyprcursor
+    pkg-config
+    openssl
+    gcc
+    gh
     #FONTS
-    pkgs.nerd-fonts.fira-mono
-    pkgs.nerd-fonts.fira-code
+    nerd-fonts.fira-mono
+    nerd-fonts.fira-code
+    # MISC
+    rofi
+    rose-pine-hyprcursor
+    hyprpaper
+    zoxide
+    wl-clipboard
+    fastfetch
   ];
 
   fonts = {
