3fd28808a263c5ca279540a38ea2b1413d26aa29
Vincent Brodin <vincent.brodin21@gmail.com>
2026-01-01 20:25:37 +0100

    added zellij

--- nixos/home.nix
+++ nixos/home.nix
@@ -63,6 +63,7 @@
     #MISC
     rofi
     rofi-network-manager
+    rofi-bluetooth
     rose-pine-hyprcursor
     hyprpaper
     fastfetch
@@ -77,6 +78,7 @@
     hyprshot
     bruno
     gemini-cli
+    zellij
   ];
 
   fonts = {
